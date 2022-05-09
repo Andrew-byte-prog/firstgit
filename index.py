5278a684bc966084aaa59498ab1e5c03ee13ced6
Andrew <andTR@protonmail.com>
2022-05-08 19:16:10 -0600

    añadimos un script de python

---
+++ index.py
@@ -0,0 +1,9 @@
+from math import e
+
+numero = float(input("Ingrese un numero: "))
+print("""
+        El numero va a ser multiplicado
+        por el numero "e"
+        """)
+
+print(f"{e*numero}")
